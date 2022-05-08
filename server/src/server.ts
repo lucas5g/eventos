189e7577c09cd81b2db0261176f6a28e5f5d0df3
lucas5g <lucasdesousa19@hotmail.com>
2022-05-08 18:53:29 -0300

    create structure

--- server/src/server.ts
+++ server/src/server.ts
@@ -16,4 +16,4 @@ app.get('/', (req, res) => {
 })
 
 
-app.listen(process.env.PORT || 8000, (() => console.log(`Server run http://localhost:8000`)))
+app.listen(process.env.PORT || 8000, (() => console.log('HTTP server running')))
