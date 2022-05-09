81b7fa9a1ecd9c2d8b4dbc21c8fb82b605093ea0
lucas5g <lucasdesousa19@hotmail.com>
2022-05-09 13:08:04 -0300

    fix crud user

--- src/pages/api/usuarios/[id].ts
+++ src/pages/api/usuarios/[id].ts
@@ -1,4 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next'
+import bcrypt from 'bcryptjs'
+
 import { prisma } from '../../../config/prisma'
 import { auth } from '../../../middleware/auth'
 
@@ -22,5 +24,61 @@ export default async function users(req: NextApiRequest, res: NextApiResponse) {
         })
 
         res.send(user)
+    }
+
+    if (req.method === 'PUT') {
+
+        const { name, email, password, profile, unity } = req.body
+
+        if (!name || !email || !profile || !unity) {
+            return res
+                .status(401)
+                .json({ msg: 'Todos os campos são obrigatórios' })
+        }
+
+        if (password) {
+            const salt = await bcrypt.genSalt(10);
+            const user = await prisma.user.update({
+                where: {
+                    id: Number(req.query.id)
+                },
+                data: {
+                    name,
+                    email,
+                    password: await bcrypt.hash(password, salt),
+                    profile,
+                    unity,
+                },
+                select: {
+                    name: true,
+                    email: true,
+                    profile: true,
+                    unity: true
+                }
+
+            })
+
+            return res.json(user)
+        }
+        const user = await prisma.user.update({
+            where: {
+                id: Number(req.query.id)
+            },
+            data: {
+                name,
+                email,
+                profile,
+                unity,
+            },
+            select: {
+                name: true,
+                email: true,
+                profile: true,
+                unity: true
+            }
+
+        })
+
+        return res.json(user)
     }
 }
--- src/pages/api/usuarios/index.ts
+++ src/pages/api/usuarios/index.ts
@@ -1,4 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next'
+import bcrypt from 'bcryptjs'
+
 import { prisma } from '../../../config/prisma'
 import { auth } from '../../../middleware/auth'
 
@@ -6,7 +8,7 @@ export default async function users(req: NextApiRequest, res: NextApiResponse) {
 
     //@ts-ignore
     auth(req, res)
-  
+
     if (req.method === 'GET') {
         const users = await prisma.user.findMany({
             select: {
@@ -19,5 +21,53 @@ export default async function users(req: NextApiRequest, res: NextApiResponse) {
         })
 
         res.send(users)
+    }
+
+
+    if (req.method === 'POST') {
+        const { name, email, password, profile, unity } = req.body
+
+        if (!name || !email || !password || !profile || !unity) {
+            return res
+                .status(401)
+                .json({ msg: 'Todos os campos são obrigatórios' })
+        }
+
+
+        const emailExist = await prisma.user.findFirst({
+            where: {
+                email
+            }
+        })
+
+        if (emailExist) {
+            return res
+                .status(401)
+                .json({ msg: 'E-mail já cadastrado!' })
+        }
+
+        // return res.send({ name, email, password, profile, unity })
+
+        const salt = await bcrypt.genSalt(10);
+
+        const user = await prisma.user.create({
+            data: {
+                name,
+                email,
+                password: await bcrypt.hash(password, salt),
+                profile,
+                unity
+            },
+            select: {
+                id: true,
+                name: true,
+                email: true,
+                profile: true,
+                unity: true
+            }
+        })
+
+
+        res.json(user)
     }
 }
