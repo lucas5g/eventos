e779108c39a9949805bf2f4cc5cc03f34af357e7
lucas5g <lucasdesousa19@hotmail.com>
2022-05-09 04:08:37 -0300

    withou back

--- server/src/helpers/jwt.mjs
+++
@@ -1,9 +0,0 @@
-import jwt from 'jsonwebtoken'
-
-export function generateAccessToken({ email }) {
-    const token = jwt.sign({
-        email,
-    }, process.env.TOKEN_SECRET)
-
-    return token
-}
