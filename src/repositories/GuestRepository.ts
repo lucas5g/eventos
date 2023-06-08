659f0101e93085ed1696a169cbe54d7167eedbf9
lucas5g <lucas.sousa@codificar.com.br>
2023-06-08 13:10:37 -0300

    test filter

--- src/repositories/GuestRepository.ts
+++ src/repositories/GuestRepository.ts
@@ -3,7 +3,7 @@ import { prisma } from "../libs/prisma";
 export class GuestRepository {
   static async findMany({profile, unity}:{profile?:string, unity?:"BH" | "Contagem" | "EPSA" | "Gutierrez" | "NovaLima" | "SIC"}) {
     if(profile === 'Admin'){
-      return await prisma.guest.findMany({ })    
+      return await prisma.guest.findMany()    
     }
     return await prisma.guest.findMany({
       where:{
